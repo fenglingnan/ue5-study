dd2d3fb2ea35b40642ddc0d284b7ec3627ccb8bd
lin <185975315@qq.com>
2022-08-31 14:58:18 +0800

    初始化

---
+++ Source/gam1e1/gam1e1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "gam1e1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gam1e1, "gam1e1" );
---
+++ Source/gam1e1/gam1e1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gam1e1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM1E1_API Agam1e1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
